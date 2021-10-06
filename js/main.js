efe10497cbe0736e9d485866df35549ef31d3e3c
Obyed <vratar1001@gmail.com>
2021-10-06 22:11:25 +0300

    Module2-task1 Написал функцию возвращающая случайное число с плавающей точкой из переданного диапазона включительно.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function getRandomNumber(min, max) {
+  return Math.floor(min + (Math.random() * max));
+}
+
+console.log(getRandomNumber(1, 9.002220));
+
+
+//Случайное целое число от min до max
+//https://learn.javascript.ru/task/random-int-min-max
