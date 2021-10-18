a9a4708eca711bf64960a4a30daa73b071fb9ca9
Obyed <vratar1001@gmail.com>
2021-10-18 22:12:55 +0300

    Module 4-task 1

---
+++ js/const.js
@@ -0,0 +1,8 @@
+export const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+export const CHECKIN = ['12:00', '13:00', '14:00'];
+export const CHECKOUT = ['12:00', '13:00', '14:00'];
+export const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+export const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
--- js/main.js
+++ js/main.js
@@ -1,63 +1,38 @@
+import {CHECKIN, CHECKOUT, FEATURES, PHOTOS, TYPES} from '../js/const.js';
+import {getRandomPositiveInteger} from '../js/utils/get-random-positive-integer.js';
+import {getRandomPositiveFloat} from '../js/utils/get-random-positive-float.js';
 
-function getRandomNumber(min, max) {
-    let rand = Math.floor(min + (Math.random() * max));
-    return rand.toFixed(2);
-  }
-  
-  console.log(getRandomNumber(1, 20.99));
-  
-  
-  //Случайное целое число от min до max
-  //https://learn.javascript.ru/task/random-int-min-max
-  
-const author =
-  {
-    avatar: 'img/avatars/user{{01}}.png',
-    avatar: 'img/avatars/user{{02}}.png',
-    avatar: 'img/avatars/user{{03}}.png',
-    avatar: 'img/avatars/user{{04}}.png',
-    avatar: 'img/avatars/user{{05}}.png',
-    avatar: 'img/avatars/user{{06}}.png',
-    avatar: 'img/avatars/user{{07}}.png',
-    avatar: 'img/avatars/user{{08}}.png',
-    avatar: 'img/avatars/user{{09}}.png',
-    avatar: 'img/avatars/user{{10}}.png',
-  }
-;
-for (let key in author) {
-  alert(author[key]);
-}
-const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-
-function getRandomItem(types) {
-  return types[Math.floor(Math.random() * types.length)];
+function getAuthor(){
+  const randomValue = getRandomPositiveInteger(0, 10);
+  let value = randomValue < 10 ? `0${randomValue}` : randomValue;
+  return `img/avatars/user${value}.png`;
 }
 
-const offer = {
-  title: 'Не понятно что придумать',
-  address: '{{location.lat}}, {{location.lng}}',
-  price: Math.random(),
-  type: getRandomItem(types),
-  rooms: Math.random(),
-  guests: Math.random(),
-  checkin: '',
-  checkout: '',
-  features: [],
-  description: 'Описание помещение',
-  photos: [
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-  ],
+function getRandomItem(items) {
+  const index = getRandomPositiveInteger(0, items.length);
+  return index[items];
 };
-const location = {
-  lat: function (min, max) {
-    min = 35.65000;
-    max = 35.70000;
-    return Math.random() * (max - min) + min;
-  },
-  lng: function (min, max) {
-    min = 139.70000;
-    max = 139.80000;
-  }
-};
+
+function getOffer() {
+  const value = {
+    title: 'Не понятно что придумать',
+    address: `${getRandomPositiveFloat(35.65000, 35.70000)}, ${139.70000, 139.80000}`,
+    price: `${getRandomPositiveFloat(0, 1000000)}`,
+    type: getRandomItem(TYPES),
+    rooms: `${getRandomPositiveInteger(1, 5)}`,
+    guests: `${getRandomPositiveInteger(1, 20)}`,
+    checkin: `${getRandomItem(CHECKIN)}`,
+    checkout: `${getRandomItem(CHECKOUT)}`,
+    features: `${getRandomItem(FEATURES)}`,
+    description: 'Описание помещение',
+    photos: `${getRandomItem(PHOTOS)}`,
+  };
+  return value;
+}
+
+debugger;
+const author = getAuthor();
+console.log(author);
+const offer = getOffer();
+console.log(offer);
+
