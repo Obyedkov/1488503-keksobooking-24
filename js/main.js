520f53cac5cc4d39cde0dbfd8f854866067ee313
Obyed <vratar1001@gmail.com>
2021-10-21 20:53:05 +0300

    Module 4-task 1 Написал функцию getArray так же добавил в функцию getOffer photos и features

--- js/main.js
+++ js/main.js
@@ -11,7 +11,26 @@ function getAuthor() {
 function getRandomItem(items) {
   const index = getRandomPositiveInteger(0, items.length);
   return index[items];
-};
+}
+function getArray(features) {
+  const maxLength = features.length;
+  const lengthOfArray = getRandomNumber(1, maxLength);
+  const array = [];
+
+  while (array.length < lengthOfArray) {
+    const indexOfEl = getRandomNumber(0, maxLength - 1);
+    const el = features[indexOfEl];
+
+    if (!array.includes(el)) {
+      array.push(el);
+    }
+  }
+  return array;
+
+  function getRandomNumber(from, to) {
+    return Math.floor(Math.random() * (to - from + 1)) + from;
+  }
+}
 
 function getOffer() {
   const value = {
@@ -24,9 +43,9 @@ function getOffer() {
     guests: `${getRandomPositiveInteger(1, 20)}`,
     checkin: `${getRandomItem(CHECKIN)}`,
     checkout: `${getRandomItem(CHECKOUT)}`,
-    features: `${getRandomItem(FEATURES)}`,
+    features: `${getArray(FEATURES)}`,
     description: 'Описание помещение',
-    photos: `${getRandomItem(PHOTOS)}`,
+    photos: `${getArray(PHOTOS)}`,
     location: {
       lat: `${getRandomPositiveFloat(35.65000, 35.70000)}`,
       lng: `${getRandomPositiveFloat(139.70000, 139.80000)}`,
